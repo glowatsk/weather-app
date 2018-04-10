1ce6e52c5d61acb3dad80388c8b723bc50129135
glowatsk <glowatsk@gmail.com>
2018-04-10 12:11:10 -0600

    Adding API key for geocoding

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const request = require('request');
+const yargs = require('yargs');
+const key = require('./api.js')
+
+API_KEY = key.API_KEY
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url:'https://maps.googleapis.com/maps/api/geocode/json?address=',
-    JSON: true
-
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key${API_KEY}`,
+  json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+  if (error) {
+    console.log('Unable to connect to Google servers.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address.');
+  } else if (body.status === 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
 });
